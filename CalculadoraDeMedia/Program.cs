f234d532e993557cd9d30e7142eed40a5d55aba3
Eduardo Silva <eduardoalsilva45@gmail.com>
2025-08-15 22:52:54 -0300

    Adicionado o exercicio CalculadoraDeMedia

---
+++ CalculadoraDeMedia/Program.cs
@@ -0,0 +1,43 @@
+﻿// Calcula a média de 3 notas de um aluno.
+
+using System.Net.Http.Headers;
+
+List<double> notas = new List<double>();
+
+Console.WriteLine("Calculadora de média. Entre com as notas");
+Console.WriteLine("As notas devem ser de 0 a 10");
+
+double nota;
+double somaNotas = 0;
+
+// Recebe as notas
+while (notas.Count() < 3)
+{
+    Console.WriteLine($"Digite a nota da avaliação {notas.Count() + 1}");
+    try
+    {
+        nota = double.Parse(Console.ReadLine());
+        if (nota is >= 0 and <= 10)
+        {
+            notas.Add(nota);
+            // Soma as notas
+            somaNotas += notas[notas.Count() - 1];
+        }
+        else
+        {
+            Console.WriteLine("A nota deve estar entre 0 e 10");
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nota incorreta. Tente novamente");
+    }
+
+}
+
+// Calcula a media
+double media = somaNotas / notas.Count();
+
+bool aprovado = media >= 7;
+
+Console.WriteLine($"Sua média: {media:F1}. Você foi {(aprovado ? "aprovado" : "reprovado")}.");
